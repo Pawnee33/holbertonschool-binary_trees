48783bc0698a51a814ff4cd8b88ef1a1e804627e
Pawnee <pawneedefize@hotmail.com>
2025-12-08 14:26:40 +0000

    add task 5 binary_tree_is_root

--- binary_trees.h
+++ binary_trees.h
@@ -23,5 +23,6 @@ typedef struct binary_tree_s
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_root(const binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
