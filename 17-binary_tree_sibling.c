a38937294e4949ad53fd3d466fe36ff84a755796
Pawnee <pawneedefize@hotmail.com>
2025-12-09 14:18:33 +0000

    remove t in the description binary_tree_sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
-* binary_tree_stibling - finds the sibling of a node
+* binary_tree_sibling - finds the sibling of a node
 * @node: pointer to the node to find the sibling
 *
 * Return: The sibling node or NULL if there is no sibling
